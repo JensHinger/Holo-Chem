ae8890f3e064a5ce8877c11c61d4c9bcc9881ca5
Jens Hinger <hinger.jens@gmail.com>
2022-02-01 15:23:14 +0100

    first spawnsystem try

---
+++ Holo-Chem-Unity/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject toSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObject();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        BoxCollider box = toSpawn.GetComponent<BoxCollider>();
+
+        var spawn = gameObject.transform.position;
+        var hitColliders = Physics.OverlapSphere(spawn, box.size.x /4); 
+
+        Debug.Log(hitColliders.Length + " size: " + box.size.x);
+
+        if(hitColliders.Length == 2)
+        {
+            SpawnObject();
+        }
+    }
+
+    void SpawnObject()
+    {
+         Object.Instantiate(toSpawn, gameObject.transform.position, toSpawn.transform.rotation);
+    }
+}
