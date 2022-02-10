16912e36cebeba39a9c84f45c9ef7acc19aef419
Jens Hinger <hinger.jens@gmail.com>
2022-02-10 16:29:25 +0100

    added Rotation

--- Holo-Chem-Unity/Assets/RotateShelves.cs
+++ Holo-Chem-Unity/Assets/RotateShelves.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class RotateShelves : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float timeToRotate = 2f;
+    private bool rotate = false;
+
+    private float blockTimer;
+    private bool blockRotate = false;
+    IEnumerator Rotate()
     {
-        
+        Quaternion fromAngle = transform.rotation;
+        Quaternion toAngle = Quaternion.Euler(transform.eulerAngles + Vector3.up * 180);
+
+        for(var t = 0f; t < 1; t+= Time.deltaTime / timeToRotate)
+        {
+            transform.rotation = Quaternion.Slerp(fromAngle, toAngle, t);
+            yield return null;
+        }
+
     }
 
-    // Update is called once per frame
+    public void setRotate()
+    {
+        rotate = true;
+    }
+
     void Update()
     {
-        transform.Rotate(Vector3.up * 90 * Time.deltaTime);
+        if (rotate && !blockRotate)
+        {
+            rotate = false;
+            blockRotate = true;
+            blockTimer = timeToRotate + 0.5f;
+            StartCoroutine(Rotate());
+        }
+
+        if (blockRotate)
+        {
+            blockTimer -= Time.deltaTime;
+        }
+
+        if(blockTimer <= 0f)
+        {
+            blockRotate = false;
+        }
+
     }
 }
